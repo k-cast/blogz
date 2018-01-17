2854e5c51baba4edf2f1be5edf3b3a1919b648ed
Kyle Casteline <kcasteli@mail.usf.edu>
2018-01-16 19:31:57 -0500

    everything works

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:3306/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "build-a-blog-secret-key"
 db = SQLAlchemy(app)
 
+
 class Entry(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(180))
     body = db.Column(db.String(1000))
+    created = db.Column(db.DateTime)
 
     def __init__(self, title, body ):
         self.title = title
         self.body = body
+    
+    def is_valid(self):
+        if self.title and self.body:
+            return True
+        else:
+            return False
 
 @app.route("/")
 def index():
@@ -22,26 +32,37 @@ def index():
 
 @app.route("/blog")
 def display_blog_entries():
+
     entry_id = request.args.get('id')
     if (entry_id):
         entry = Entry.query.get(entry_id)
-        return render_template('blog.html', title="Blog Entry", entry=entry)
-
-    sort = request.args.get('sort')
-    if (sort=="newest"):
-        all_entries = Entry.query.order_by(Entry.created.desc()).all()
+        return render_template('singlepost.html', title="Blog Entry", entry=entry)
     else:
         all_entries = Entry.query.all()   
     return render_template('blog.html', title="All Entries", all_entries=all_entries)
 
+
 @app.route('/newpost', methods=['GET', 'POST'])
 def newpost():
+
     if request.method == 'POST':
         new_post_title = request.form['title']
         new_post_body = request.form['body']
         new_post = Entry(new_post_title, new_post_body)
-    else:
-        return render_template('newpost.html', title="CREATE A BLOG POST")
+
+        if new_post.is_valid():
+            db.session.add(new_post)
+            db.session.commit()
+
+            url = "/blog?id=" + str(new_post.id)
+            return redirect(url)
+        else:
+            flash("NOT A VALID BLOG POST")
+            return render_template('newpost.html', title="Create new blog post",
+                new_post_title=new_post_title, new_post_body=new_post_body)
+
+    else: 
+        return render_template('newpost.html', title="Create new blog post!")
 
 if __name__ == '__main__':
     app.run()
